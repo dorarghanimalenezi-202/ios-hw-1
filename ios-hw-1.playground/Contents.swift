16fbccb8c2c7443c414baeb5119e0d7675d0f2b3
dorarghanimalenezi202 <dorar.alenezi.2@gmail.com>
2020-06-14 23:58:47 +0300

    ios hw1 is done
    hw1 done

--- ios-hw-1.playground/Contents.swift
+++ ios-hw-1.playground/Contents.swift
@@ -5,12 +5,12 @@
  
  */
 
-let name = "Ahmad"
+var name = "Ahmad"
 
 name = "Salem"
 
 
-var myWeight = 60
+var myWeight = 60.0
 var FriendWeight = 80.5
 var height = 1.5
 
@@ -38,18 +38,27 @@ var bmi = myWeight * (height * height)
  */
 
 //Person 1
-var firstName1 = ""
-var lastName1 = ""
-var email1 = ""
-var phoneNumber1 = ""
-var age1 = 0
-var country1 = ""
-var area1 = ""
-var password = ""
-var isKuwaiti = false
+var firstName1 = "dorar"
+var lastName1 = "alenezi"
+var email1 = "dorar.alenezi.2@gmail.com"
+var phoneNumber1 = "90992525"
+var age1 = 15
+var country1 = "kuwait"
+var area1 = "sulaibikat"
+var password = "12345"
+var isKuwaiti = true
 
 //Person 2
 
+var firstName2 = "dana"
+var lastName2 = "khaled"
+var email2 = "dana.kh@gmail.com"
+var phoneNumber2 = "99734539"
+var age2 = 15
+var country2 = "kuwait"
+var area2 = "hadiah"
+var password2 = "33345"
+var isKuwaiti2 = true
 
 
 
@@ -60,14 +69,15 @@ var isKuwaiti = false
 
 
 //Person 3
-
-
-
-
-
-
-
-
+var firstName3 = "fayza"
+var lastName3 = "jassim"
+var email3 = "fayzajassim.234@hotmail.com"
+var phoneNumber3 = "90994567"
+var age3 = 15
+var country3 = "kuwait"
+var area3 = "adan"
+var password3 = "1234fa"
+var isKuwaiti3 = true
 
 /*
 - - - -
@@ -79,6 +89,22 @@ var isKuwaiti = false
  * قارن اذا عمر الاشخاص اصغر من ١٨
  * قارن اذا عمر الاشخاص يساوي  ١٧
  * قارن اذا عمر الاشخاص لا يساوي  ١٧
+ */
+var age1isKuwaiti = true
+var age2iskuwaiti = true
+var  age3iskuwaiti = true
+
+
+age1 == age2||age2 == age3||age3 == age1
+ age1  != age2||age2 != age3||age3 != age1
+
+ area1 == area2||area2 == area3||area3 == area1
+ area1 != area2||area2 != area3||area3 != area1
+ 
+age1 > 18 && age2 > 18 && age3 > 18
+age1 < 18 && age2 < 18 && age3 < 18
 
  
- */
+ age1 == 17 && age2 == 17 && age3 == 17
+ age1 != 17 && age2 != 17 && age3 != 17
+ 
